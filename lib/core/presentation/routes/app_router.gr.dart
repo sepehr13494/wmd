95974b5e771a6a2ca3a3ad71e4e6a1268acccffc
Sumit Sharma <sumitsharma180@gmail.com>
2022-09-19 17:37:08 +0530

    iOS configurations and files

---
+++ lib/core/presentation/routes/app_router.gr.dart
@@ -0,0 +1,105 @@
+// **************************************************************************
+// AutoRouteGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouteGenerator
+// **************************************************************************
+//
+// ignore_for_file: type=lint
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:auto_route/auto_route.dart' as _i5;
+import 'package:flutter/material.dart' as _i6;
+
+import '../../../features/authentication/presentation/pages/login_page.dart'
+    as _i2;
+import '../../../features/authentication/presentation/pages/register_page.dart'
+    as _i3;
+import '../../../features/main/presentation/pages/main_page.dart' as _i4;
+import '../../../features/splash/presentation/pages/splash_page.dart' as _i1;
+
+class AppRouter extends _i5.RootStackRouter {
+  AppRouter([_i6.GlobalKey<_i6.NavigatorState>? navigatorKey])
+      : super(navigatorKey);
+
+  @override
+  final Map<String, _i5.PageFactory> pagesMap = {
+    SplashRoute.name: (routeData) {
+      return _i5.MaterialPageX<dynamic>(
+          routeData: routeData, child: const _i1.SplashPage());
+    },
+    LoginRoute.name: (routeData) {
+      return _i5.MaterialPageX<dynamic>(
+          routeData: routeData, child: const _i2.LoginPage());
+    },
+    RegisterRoute.name: (routeData) {
+      return _i5.MaterialPageX<dynamic>(
+          routeData: routeData, child: const _i3.RegisterPage());
+    },
+    MainRoute.name: (routeData) {
+      final args =
+          routeData.argsAs<MainRouteArgs>(orElse: () => const MainRouteArgs());
+      return _i5.MaterialPageX<dynamic>(
+          routeData: routeData,
+          child: _i4.MainPage(key: args.key, title: args.title));
+    }
+  };
+
+  @override
+  List<_i5.RouteConfig> get routes => [
+        _i5.RouteConfig(SplashRoute.name, path: '/'),
+        _i5.RouteConfig(LoginRoute.name, path: '/login-page'),
+        _i5.RouteConfig(RegisterRoute.name, path: '/register-page'),
+        _i5.RouteConfig(MainRoute.name, path: '/main-page')
+      ];
+}
+
+/// generated route for
+/// [_i1.SplashPage]
+class SplashRoute extends _i5.PageRouteInfo<void> {
+  const SplashRoute() : super(SplashRoute.name, path: '/');
+
+  static const String name = 'SplashRoute';
+}
+
+/// generated route for
+/// [_i2.LoginPage]
+class LoginRoute extends _i5.PageRouteInfo<void> {
+  const LoginRoute() : super(LoginRoute.name, path: '/login-page');
+
+  static const String name = 'LoginRoute';
+}
+
+/// generated route for
+/// [_i3.RegisterPage]
+class RegisterRoute extends _i5.PageRouteInfo<void> {
+  const RegisterRoute() : super(RegisterRoute.name, path: '/register-page');
+
+  static const String name = 'RegisterRoute';
+}
+
+/// generated route for
+/// [_i4.MainPage]
+class MainRoute extends _i5.PageRouteInfo<MainRouteArgs> {
+  MainRoute({_i6.Key? key, String title = "title"})
+      : super(MainRoute.name,
+            path: '/main-page', args: MainRouteArgs(key: key, title: title));
+
+  static const String name = 'MainRoute';
+}
+
+class MainRouteArgs {
+  const MainRouteArgs({this.key, this.title = "title"});
+
+  final _i6.Key? key;
+
+  final String title;
+
+  @override
+  String toString() {
+    return 'MainRouteArgs{key: $key, title: $title}';
+  }
+}
