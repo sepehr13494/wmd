e58f6cf84c6f2eac69ab3d18b45f6fbe4cf843f2
sepehr <marash.sepehr@gmail.com>
2022-10-31 14:42:42 +0330

    feat: pie chart

--- lib/features/dashboard/main_dashbaord/presentation/pages/dashboard_main_page.dart
+++ lib/features/dashboard/main_dashbaord/presentation/pages/dashboard_main_page.dart
@@ -3,6 +3,7 @@ import 'package:wmd/core/presentation/widgets/app_stateless_widget.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:wmd/features/authentication/login_signup/presentation/widgets/custom_app_bar.dart';
 import 'package:wmd/features/dashboard/main_dashbaord/presentation/widget/line_chart.dart';
+import 'package:wmd/features/dashboard/main_dashbaord/presentation/widget/pie_chart_sample.dart';
 import 'package:wmd/features/dashboard/main_dashbaord/presentation/widget/random_map.dart';
 
 class DashboardMainPage extends AppStatelessWidget {
@@ -17,6 +18,7 @@ class DashboardMainPage extends AppStatelessWidget {
           children: [
             RandomWorldMapGenrator(),
             LineChartSample2(),
+            PieChartSample2(),
           ].map((e) => Padding(padding: const EdgeInsets.symmetric(vertical: 8,horizontal: 16),child: e)).toList(),
         ),
       ),
