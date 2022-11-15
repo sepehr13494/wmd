036c419e993987af5ab6713f0cd26cf94a4f6de9
Hashan Jayalath <hashan.jayalath@nagarro.com>
2022-11-15 13:52:19 +0530

    Alignment issue fixed

--- lib/core/presentation/widgets/width_limitter.dart
+++ lib/core/presentation/widgets/width_limitter.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 class WidthLimiterWidget extends StatelessWidget {
   final Widget child;
   final double width;
-  const WidthLimiterWidget({Key? key, required this.child, this.width = 500}) : super(key: key);
+  const WidthLimiterWidget({Key? key, required this.child, this.width = 500})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return Align(
+      alignment: Alignment.topCenter,
       child: Container(
         constraints: BoxConstraints(maxWidth: width),
         child: child,
