ff42f6c7cf185bcdf27dac80ca48f256ff610de6
sepehr <marash.sepehr@gmail.com>
2022-11-16 14:05:48 +0330

    feat: fixing dotEnv from testcases

--- test/features/dashboard/main_dashboard/data/data_sources/main_dashboard_remote_data_source_impl_test.dart
+++ test/features/dashboard/main_dashboard/data/data_sources/main_dashboard_remote_data_source_impl_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:wmd/core/data/network/server_request_manager.dart';
@@ -14,7 +15,8 @@ import 'package:wmd/features/dashboard/user_status/data/models/user_status.dart'
 
 import '../../../../../core/data/network/error_handler_middleware_test.mocks.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: 'assets/env/.env');
   late MockErrorHandlerMiddleware mockErrorHandlerMiddleware;
   late MainDashboardRemoteDataSourceImpl mainDashboardRemoteDataSourceImpl;
 
@@ -40,7 +42,7 @@ void main() {
           await mainDashboardRemoteDataSourceImpl.userNetWorth(NetWorthParams.tNetWorthParams);
       //assert
       verify(mockErrorHandlerMiddleware.sendRequest(tGetNetWorthOptions));
-      expect(result, UserStatus.fromJson(NetWorthResponseObj.tNetWorthResponseObj.toJson()));
+      expect(result, NetWorthResponseObj.fromJson(NetWorthResponseObj.tNetWorthResponseObj.toJson()));
     });
 
     test('should throws ServerException when API call is not successful',
