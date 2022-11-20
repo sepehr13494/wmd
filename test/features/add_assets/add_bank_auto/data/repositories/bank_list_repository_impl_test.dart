b8dd842035d67624ee4f71d57a97e61ffc1d6c13
Ibrahim Mert Gul <i.mertgul.c@tfoco.com>
2022-11-20 16:50:50 +0300

    add usecase tests

--- test/features/add_assets/add_bank_auto/data/repositories/bank_list_repository_impl_test.dart
+++ test/features/add_assets/add_bank_auto/data/repositories/bank_list_repository_impl_test.dart
@@ -8,10 +8,6 @@ import 'package:wmd/core/error_and_success/failures.dart';
 import 'package:wmd/features/add_assets/add_bank_auto/data/data_sources/bank_list_data_source.dart';
 import 'package:wmd/features/add_assets/add_bank_auto/data/models/bank_list_response.dart';
 import 'package:wmd/features/add_assets/add_bank_auto/data/repository/bank_list_repository_impl.dart';
-import 'package:wmd/features/add_assets/add_bank_auto/domain/entity/bank_entity.dart';
-import 'package:wmd/features/add_assets/add_basic_cash_asset/data/models/bank_save_response_model.dart';
-import 'package:wmd/features/add_assets/add_basic_cash_asset/data/repositories/bank_repository_impl.dart';
-import 'package:wmd/features/add_assets/add_basic_cash_asset/domain/use_cases/post_bank_details_usecase.dart';
 
 import 'bank_list_repository_impl_test.mocks.dart';
 
@@ -27,7 +23,7 @@ void main() {
 
   final tServerException = ServerException(message: 'test server message');
 
-  group('test for postBankDetails in BankRepository', () {
+  group('test for getBankList in BankListRepository', () {
     test(
       'should return bank response data when the call to bank list remote data source is successful',
       () async {
@@ -57,6 +53,42 @@ void main() {
         final result = await bankListRepositoryImpl.getBankList(NoParams());
         // assert
         verify(mockBankListRemoteDataSource.getBankList(NoParams()));
+
+        expect(result,
+            equals(Left(ServerFailure(message: tServerException.message))));
+      },
+    );
+  });
+  group('test for getPopularBankList in BankListRepository', () {
+    test(
+      'should return bank response data when the call to bank list remote data source is successful',
+      () async {
+        final list = List.generate(
+            3, (index) => BankResponse.fromJson(BankResponse.tBankResponse));
+        // arrange
+        when(mockBankListRemoteDataSource.getPopularBankList(any))
+            .thenAnswer((_) async => list);
+        // act
+        final result = await bankListRepositoryImpl.getPopularBankList(3);
+        // assert
+        verify(mockBankListRemoteDataSource.getPopularBankList(3));
+        expect(
+          result,
+          equals(Right(list)),
+        );
+      },
+    );
+
+    test(
+      'should return server failure on server exception',
+      () async {
+        // arrange
+        when(mockBankListRemoteDataSource.getPopularBankList(any))
+            .thenThrow(tServerException);
+        // act
+        final result = await bankListRepositoryImpl.getPopularBankList(3);
+        // assert
+        verify(mockBankListRemoteDataSource.getPopularBankList(3));
 
         expect(result,
             equals(Left(ServerFailure(message: tServerException.message))));
