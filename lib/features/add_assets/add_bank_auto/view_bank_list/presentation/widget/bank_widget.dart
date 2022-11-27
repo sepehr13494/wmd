55525c70b3d7a2194b5de882d61e022a06bf81a3
Ibrahim Mert Gul <i.mertgul.c@tfoco.com>
2022-11-27 10:04:37 +0300

    Move Plaid button

--- lib/features/add_assets/add_bank_auto/view_bank_list/presentation/widget/bank_widget.dart
+++ lib/features/add_assets/add_bank_auto/view_bank_list/presentation/widget/bank_widget.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:wmd/core/presentation/bloc/base_cubit.dart';
-import 'package:wmd/core/presentation/bloc/bloc_helpers.dart';
 import 'package:wmd/core/presentation/widgets/app_stateless_widget.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:wmd/core/util/constants.dart';
-import 'package:wmd/features/add_assets/add_bank_auto/plaid_integration/presentation/manager/plaid_cubit.dart';
-import 'package:wmd/injection_container.dart';
-
+import 'package:wmd/features/add_assets/add_bank_auto/plaid_integration/presentation/widget/plaid_connect_button.dart';
 import '../../domain/entity/bank_entity.dart';
 
 class BankWidget extends StatefulWidget {
@@ -46,7 +41,7 @@ class _BankWidgetState extends AppState<BankWidget> {
           leading: bank.logo == null
               ? Icon(Icons.account_balance, color: primaryColor)
               : Image.network(bank.logo!),
-          subtitle: Text(bank.provider ?? 'No provider'),
+          // subtitle: Text(bank.provider ?? 'No provider'),
           trailing: !isSelected
               ? null
               : Builder(
@@ -64,49 +59,6 @@ class _BankWidgetState extends AppState<BankWidget> {
           selected: isSelected,
           selectedColor: null,
         ),
-      ),
-    );
-  }
-}
-
-class PlaidConnectButton extends AppStatelessWidget {
-  final BankEntity bank;
-  const PlaidConnectButton(this.bank, {Key? key}) : super(key: key);
-
-  @override
-  Widget buildWidget(BuildContext context, TextTheme textTheme,
-      AppLocalizations appLocalizations) {
-    final primaryColor = Theme.of(context).primaryColor;
-    return BlocProvider(
-      create: (context) => sl<PlaidCubit>(),
-      child: BlocConsumer<PlaidCubit, PlaidState>(
-          listener:
-              BlocHelper.defaultBlocListener(listener: (context, state) {}),
-          builder: (context, state) {
-            if (state is PlaidInitialState || state is ErrorState) {
-              return InkWell(
-                onTap: () {
-                  context.read<PlaidCubit>().linkPlaidAccount(bank);
-                },
-                child: _buildContainerWithborder('Connect', primaryColor),
-              );
-            } else if (state is PlaidLinkSuccess) {
-              return _buildContainerWithborder('Connected');
-            } else {
-              return _buildContainerWithborder('...');
-            }
-          }),
-    );
-  }
-
-  Container _buildContainerWithborder(String message, [Color? borderColor]) {
-    return Container(
-      decoration: BoxDecoration(
-        border: borderColor != null ? Border.all(color: borderColor) : null,
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-        child: Text(message),
       ),
     );
   }
