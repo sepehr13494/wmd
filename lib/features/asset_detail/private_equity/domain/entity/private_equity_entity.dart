bf41a85b44b5d3378b8141a5dd9beeab4318abe1
Ibrahim Mert Gul <i.mertgul.c@tfoco.com>
2022-12-11 15:15:20 +0300

    add privateEquity models

---
+++ lib/features/asset_detail/private_equity/domain/entity/private_equity_entity.dart
@@ -0,0 +1,66 @@
+import 'package:equatable/equatable.dart';
+
+class PrivateEquityEntity extends Equatable {
+  const PrivateEquityEntity({
+    required this.investmentName,
+    required this.investmentAmount,
+    required this.investmentDate,
+    required this.wealthManager,
+    required this.marketValue,
+    required this.valuationDate,
+    required this.id,
+    required this.type,
+    required this.isActive,
+    required this.country,
+    required this.region,
+    required this.currencyCode,
+    required this.portfolioContribution,
+  });
+
+  final String investmentName;
+  final double investmentAmount;
+  final DateTime investmentDate;
+  final String wealthManager;
+  final double marketValue;
+  final DateTime valuationDate;
+  final String id;
+  final String type;
+  final bool isActive;
+  final String country;
+  final String region;
+  final String currencyCode;
+  final double portfolioContribution;
+
+  Map<String, dynamic> toJson() => {
+        "investmentName": investmentName,
+        "investmentAmount": investmentAmount,
+        "investmentDate": investmentDate.toIso8601String(),
+        "wealthManager": wealthManager,
+        "marketValue": marketValue,
+        "valuationDate": valuationDate.toIso8601String(),
+        "id": id,
+        "type": type,
+        "isActive": isActive,
+        "country": country,
+        "region": region,
+        "currencyCode": currencyCode,
+        "portfolioContribution": portfolioContribution,
+      };
+
+  @override
+  List<Object?> get props => [
+        investmentName,
+        investmentAmount,
+        investmentDate,
+        wealthManager,
+        marketValue,
+        valuationDate,
+        id,
+        type,
+        isActive,
+        country,
+        region,
+        currencyCode,
+        portfolioContribution,
+      ];
+}
