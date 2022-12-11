49b305ca9dd66fe2932d617d15e97e07a71ae51b
Ibrahim Mert Gul <i.mertgul.c@tfoco.com>
2022-12-11 10:31:42 +0300

    add appException to asset detail

--- lib/features/asset_detail/core/data/repositories/asset_detail_repository_impl.dart
+++ lib/features/asset_detail/core/data/repositories/asset_detail_repository_impl.dart
@@ -4,7 +4,7 @@ import 'package:dartz/dartz.dart';
 
 import '../models/get_detail_params.dart';
 import '../../domain/entities/get_detail_entity.dart';
-    
+
 import '../../domain/repositories/asset_detail_repository.dart';
 import '../data_sources/asset_detail_remote_datasource.dart';
 
@@ -13,16 +13,16 @@ class AssetDetailRepositoryImpl implements AssetDetailRepository {
 
   AssetDetailRepositoryImpl(this.remoteDataSource);
 
-    @override
-  Future<Either<Failure, GetDetailEntity>> getDetail(GetDetailParams params) async {
+  @override
+  Future<Either<Failure, GetDetailEntity>> getDetail(
+      GetDetailParams params) async {
     try {
       final result = await remoteDataSource.getDetail(params);
       return Right(result);
     } on ServerException catch (error) {
       return Left(ServerFailure.fromServerException(error));
+    } on AppException catch (error) {
+      return Left(AppFailure.fromAppException(error));
     }
   }
-  
-    
 }
-
