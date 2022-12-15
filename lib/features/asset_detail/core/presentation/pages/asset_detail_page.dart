a94c860ddd7a15ffcb4afe4d3eb648ddaa24f365
Ibrahim Mert Gul <ibrahimmertgul@gmail.com>
2022-12-15 12:57:28 +0300

    hot-fix/import_error

--- lib/features/asset_detail/core/presentation/pages/asset_detail_page.dart
+++ lib/features/asset_detail/core/presentation/pages/asset_detail_page.dart
@@ -16,7 +16,7 @@ import 'package:wmd/features/asset_detail/private_equity/domain/entity/private_e
 import 'package:wmd/features/asset_detail/private_equity/presentation/page/private_equity_page.dart';
 import 'package:wmd/features/asset_detail/real_estate/domain/entity/real_estate_entity.dart';
 import 'package:wmd/features/asset_detail/real_estate/presentation/page/real_estate_page.dart';
-import 'package:wmd/features/dashboard/main_dashbaord/presentation/widget/net_worth_base_chart.dart';
+import 'package:wmd/features/dashboard/dashboard_charts/presentation/widgets/net_worth_base_chart.dart';
 import 'package:wmd/injection_container.dart';
 import '../manager/asset_detail_cubit.dart';
 import '../../../bank_account/presentation/page/bank_account_page.dart';
