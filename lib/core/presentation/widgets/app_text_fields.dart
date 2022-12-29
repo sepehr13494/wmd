26022254cb4187b23c1da8f305beb45208dfcafa
Hashan Jayalath <hashan.jayalath@nagarro.com>
2022-12-29 16:01:27 +0400

    refactoring

--- lib/core/presentation/widgets/app_text_fields.dart
+++ lib/core/presentation/widgets/app_text_fields.dart
@@ -32,11 +32,6 @@ class CurrencyInputFormatter extends TextInputFormatter {
 
 enum TextFieldType { email, password, phone, simpleText, money, number, rate }
 
-class AlwaysDisabledFocusNode extends FocusNode {
-  @override
-  bool get hasFocus => false;
-}
-
 class AppTextFields {
   AppTextFields._();
 
