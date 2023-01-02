c6a1b314ea9ec898d1dbc0450300bd8989d189af
Ibrahim Mert Gul <ibrahimmertgul@gmail.com>
2023-01-02 13:07:24 +0300

    chart picker

--- lib/features/assets_overview/assets_overview/presentation/widgets/charts_wrapper.dart
+++ lib/features/assets_overview/assets_overview/presentation/widgets/charts_wrapper.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wmd/core/presentation/widgets/app_stateless_widget.dart';
 import 'package:wmd/core/presentation/widgets/responsive_helper/responsive_helper.dart';
+import 'package:wmd/core/util/colors.dart';
 import 'package:wmd/features/assets_overview/charts/presentation/widgets/base_chart_view.dart';
 import 'package:wmd/features/dashboard/main_dashbaord/presentation/manager/main_dashboard_cubit.dart';
 
@@ -57,9 +58,35 @@ class ChartsWrapper extends AppStatelessWidget {
               ),
               AspectRatio(
                 aspectRatio:
-                    ResponsiveHelper(context: context).isMobile ? 1.6 : 2,
+                    ResponsiveHelper(context: context).isMobile ? 1.6 : 1.5,
                 child: TabBarView(children: [
-                  const BaseAssetsOverviewChartsWidget(),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        vertical: ResponsiveHelper(context: context).biggerGap),
+                    child: Card(
+                      color: Theme.of(context).brightness == Brightness.dark
+                          ? AppColors.darkCardColorForDarkTheme
+                          : AppColors.darkCardColorForLightTheme,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                ChartPicker(
+                                  value: 0,
+                                  onChange: (value) {},
+                                ),
+                              ],
+                            ),
+                            const Expanded(
+                                child: BaseAssetsOverviewChartsWidget()),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
                   Container(),
                   Container(),
                 ]),
@@ -68,6 +95,49 @@ class ChartsWrapper extends AppStatelessWidget {
           ),
         ),
       ],
+    );
+  }
+}
+
+class ChartPicker extends AppStatelessWidget {
+  const ChartPicker({
+    Key? key,
+    required this.onChange,
+    required this.value,
+  }) : super(key: key);
+
+  final void Function(int? value) onChange;
+  final int value;
+
+  @override
+  Widget buildWidget(BuildContext context, textTheme, appLocalizations) {
+    return DropdownButton<int>(
+      items: [
+        DropdownMenuItem<int>(
+            value: 0,
+            child: Row(
+              children: [
+                Icon(
+                  Icons.bar_chart,
+                  color: Theme.of(context).primaryColor,
+                ),
+                Text(
+                  'Bar Chart',
+                  style: textTheme.bodyMedium!
+                      .apply(color: Theme.of(context).primaryColor),
+                  // textTheme.bodyMedium!.toLinkStyle(context),
+                ),
+              ],
+            ))
+      ],
+      onChanged: onChange,
+      value: value,
+      icon: Icon(
+        Icons.keyboard_arrow_down,
+        size: 15,
+        color: Theme.of(context).primaryColor,
+      ),
+      // style: textTheme.labelLarge,
     );
   }
 }
