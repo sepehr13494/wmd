e1dd1b809bdd0cfc65e9e5c31990d9aee55e0e6a
Ibrahim Mert Gul <ibrahimmertgul@gmail.com>
2023-01-18 13:44:58 +0300

    fixed line chart

--- lib/features/dashboard/dashboard_charts/presentation/widgets/bar_chart.dart
+++ lib/features/dashboard/dashboard_charts/presentation/widgets/bar_chart.dart
@@ -22,20 +22,22 @@ class BarChartMainDashboard extends StatelessWidget {
   }
 
   Widget bottomTitleWidgets(double value, TitleMeta meta) {
-    int x = (allocations.length/7).ceil();
-    return value.toInt() % x == 0 ? SideTitleWidget(
-      axisSide: meta.axisSide,
-      child: Text(
-          CustomizableDateTime.miniDate(allocations[value.toInt()].name),
-          style: const TextStyle(fontSize: 8)),
-    ) : const SizedBox();
+    int x = (allocations.length / 7).ceil();
+    return value.toInt() % x == 0
+        ? SideTitleWidget(
+            axisSide: meta.axisSide,
+            child: Text(
+                CustomizableDateTime.miniDate(allocations[value.toInt()].name),
+                style: const TextStyle(fontSize: 8)),
+          )
+        : const SizedBox();
   }
 
   Widget leftTitleWidgets(double value, TitleMeta meta) {
-    List<double> minMax = MinMaxCalculator.calculateMinyMaxY(allocations,false);
+    List<double> minMax = MinMaxCalculator.calculateMinyMaxY(allocations);
     double minY = minMax[0];
     double maxY = minMax[1];
-    double x = max(maxY.abs() , minY.abs()) / 5;
+    double x = max(maxY.abs(), minY.abs()) / 5;
     return FittedBox(
       fit: BoxFit.scaleDown,
       child: Text(
@@ -47,55 +49,63 @@ class BarChartMainDashboard extends StatelessWidget {
   }
 
   BarChartData mainData(context) {
-    List<double> minMax = MinMaxCalculator.calculateMinyMaxY(allocations,false);
+    List<double> minMax = MinMaxCalculator.calculateMinyMaxY(allocations);
     double minY = minMax[0];
     double maxY = minMax[1];
-    double x = max(maxY.abs() , minY.abs()) / 5;
-    minY = (minY/x);
-    maxY = (maxY/x);
+    double x = max(maxY.abs(), minY.abs()) / 5;
+    minY = (minY / x);
+    maxY = (maxY / x);
     double maxTotal = max(minY.abs(), maxY.abs());
     return BarChartData(
-      barTouchData: BarTouchData(
+        barTouchData: BarTouchData(
           touchTooltipData: BarTouchTooltipData(
             fitInsideVertically: true,
             fitInsideHorizontally: true,
             getTooltipItem: (group, groupIndex, rod, rodIndex) {
               final textTheme = Theme.of(context).textTheme;
               return BarTooltipItem(
-                  CustomizableDateTime.miniDateOneLine(allocations[groupIndex.toInt()].name),
+                CustomizableDateTime.miniDateOneLine(
+                    allocations[groupIndex.toInt()].name),
                 textTheme.titleSmall!,
                 textAlign: TextAlign.start,
                 children: [
                   TextSpan(
-                    // ignore: prefer_interpolation_to_compose_strings
+                      // ignore: prefer_interpolation_to_compose_strings
                       text: '\n\n' +
-                          AppLocalizations.of(context).home_dashboardCharts_legendLabel_netWorth+ "    ",
+                          AppLocalizations.of(context)
+                              .home_dashboardCharts_legendLabel_netWorth +
+                          "    ",
                       style: textTheme.titleSmall),
                   TextSpan(
-                    // ignore: prefer_interpolation_to_compose_strings
+                      // ignore: prefer_interpolation_to_compose_strings
                       text: allocations[groupIndex.toInt()]
                           .netWorth
                           .formatNumberWithDecimal(),
                       style: textTheme.titleSmall!
                           .apply(color: AppColors.chartColor)),
                   TextSpan(
-                    // ignore: prefer_interpolation_to_compose_strings
+                      // ignore: prefer_interpolation_to_compose_strings
                       text: '\n' +
-                          AppLocalizations.of(context).home_dashboardCharts_legendLabel_assets+ "         ",
+                          AppLocalizations.of(context)
+                              .home_dashboardCharts_legendLabel_assets +
+                          "         ",
                       style: textTheme.titleSmall),
                   TextSpan(
-                    // ignore: prefer_interpolation_to_compose_strings
+                      // ignore: prefer_interpolation_to_compose_strings
                       text: allocations[groupIndex.toInt()]
                           .asset
                           .formatNumberWithDecimal(),
                       style: textTheme.titleSmall!
                           .apply(color: AppColors.chartColor)),
                   TextSpan(
-                    // ignore: prefer_interpolation_to_compose_strings
-                      text: '\n' + AppLocalizations.of(context).home_dashboardCharts_legendLabel_liability+ "      ",
+                      // ignore: prefer_interpolation_to_compose_strings
+                      text: '\n' +
+                          AppLocalizations.of(context)
+                              .home_dashboardCharts_legendLabel_liability +
+                          "      ",
                       style: textTheme.titleSmall),
                   TextSpan(
-                    // ignore: prefer_interpolation_to_compose_strings
+                      // ignore: prefer_interpolation_to_compose_strings
                       text: allocations[groupIndex.toInt()]
                           .liability
                           .formatNumberWithDecimal(),
@@ -108,58 +118,65 @@ class BarChartMainDashboard extends StatelessWidget {
             tooltipBgColor: const Color.fromARGB(255, 38, 49, 52),
           ),
         ),
-      gridData: FlGridData(
-        show: true,
-        drawVerticalLine: true,
-        horizontalInterval: 1,
-        verticalInterval: 1,
-        getDrawingHorizontalLine: (value) {
-          return FlLine(
-            color: AppColors.dashBoardGreyTextColor,
-            strokeWidth: 0.3,
-          );
-        },
-        getDrawingVerticalLine: (value) {
-          return FlLine(
-            color: Theme.of(context).cardColor,
-            strokeWidth: 1,
-          );
-        },
-      ),
-      titlesData: FlTitlesData(
-        show: true,
-        rightTitles: AxisTitles(
-          sideTitles: SideTitles(showTitles: false),
-        ),
-        topTitles: AxisTitles(
-          sideTitles: SideTitles(showTitles: false),
-        ),
-        bottomTitles: AxisTitles(
-          sideTitles: SideTitles(
-            showTitles: true,
-            reservedSize: 30,
-            interval: 1,
-            getTitlesWidget: bottomTitleWidgets,
-          ),
-        ),
-        leftTitles: AxisTitles(
-          sideTitles: SideTitles(
-            showTitles: true,
-            interval: 1,
-            getTitlesWidget: leftTitleWidgets,
-            reservedSize: 42,
-          ),
-        ),
-      ),
-      borderData: FlBorderData(
+        gridData: FlGridData(
           show: true,
-          border: const Border.symmetric(
-              horizontal: BorderSide(
-                  width: 0.3, color: AppColors.dashBoardGreyTextColor))),
-        minY: minY.abs() == maxTotal ? minY : minY >= 0 ? 0 : (- (minY.abs()).ceil().toDouble()),
-        maxY: maxY.abs() == maxTotal ? maxY : maxY <= 0 ? 0 : (maxY.abs()).ceil().toDouble(),
-      barGroups: getData(x)
-    );
+          drawVerticalLine: true,
+          horizontalInterval: 1,
+          verticalInterval: 1,
+          getDrawingHorizontalLine: (value) {
+            return FlLine(
+              color: AppColors.dashBoardGreyTextColor,
+              strokeWidth: 0.3,
+            );
+          },
+          getDrawingVerticalLine: (value) {
+            return FlLine(
+              color: Theme.of(context).cardColor,
+              strokeWidth: 1,
+            );
+          },
+        ),
+        titlesData: FlTitlesData(
+          show: true,
+          rightTitles: AxisTitles(
+            sideTitles: SideTitles(showTitles: false),
+          ),
+          topTitles: AxisTitles(
+            sideTitles: SideTitles(showTitles: false),
+          ),
+          bottomTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              reservedSize: 30,
+              interval: 1,
+              getTitlesWidget: bottomTitleWidgets,
+            ),
+          ),
+          leftTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              interval: 1,
+              getTitlesWidget: leftTitleWidgets,
+              reservedSize: 42,
+            ),
+          ),
+        ),
+        borderData: FlBorderData(
+            show: true,
+            border: const Border.symmetric(
+                horizontal: BorderSide(
+                    width: 0.3, color: AppColors.dashBoardGreyTextColor))),
+        minY: minY.abs() == maxTotal
+            ? minY
+            : minY >= 0
+                ? 0
+                : (-(minY.abs()).ceil().toDouble()),
+        maxY: maxY.abs() == maxTotal
+            ? maxY
+            : maxY <= 0
+                ? 0
+                : (maxY.abs()).ceil().toDouble(),
+        barGroups: getData(x));
   }
 
   List<BarChartGroupData> getData(double x) {
@@ -169,10 +186,12 @@ class BarChartMainDashboard extends StatelessWidget {
         barsSpace: 4,
         barRods: [
           BarChartRodData(
-            toY: allocations[index].asset/x,
+            toY: allocations[index].asset / x,
             rodStackItems: [
-              BarChartRodStackItem(0, allocations[index].netWorth/x, AppColors.chartColor),
-              BarChartRodStackItem(allocations[index].netWorth/x, allocations[index].asset/x, AppColors.redChartColor),
+              BarChartRodStackItem(
+                  0, allocations[index].netWorth / x, AppColors.chartColor),
+              BarChartRodStackItem(allocations[index].netWorth / x,
+                  allocations[index].asset / x, AppColors.redChartColor),
             ],
             borderRadius: BorderRadius.zero,
           ),
