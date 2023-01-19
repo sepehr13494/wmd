d59e4a548ce20dee20b431ae5579997345815fe5
Ibrahim Mert Gul <ibrahimmertgul@gmail.com>
2023-01-19 15:44:24 +0300

    add mvp2Item filter from env

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ Future<void> main() async {
 
   const String envFor = String.fromEnvironment(
     'env',
-    defaultValue: 'qa',
+    defaultValue: 'dev',
   );
   final envFile = envInitConfig(envFor);
   await dotenv.load(fileName: envFile);
