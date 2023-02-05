dd73ad7463583ce4636a8f73491b9eac3887e2ad
Ibrahim Mert Gul <ibrahimmertgul@gmail.com>
2023-02-05 12:22:47 +0300

    able to send accountId now

--- lib/features/add_assets/custodian_bank_auth/data/models/get_custodian_bank_status_params.dart
+++ lib/features/add_assets/custodian_bank_auth/data/models/get_custodian_bank_status_params.dart
@@ -2,12 +2,23 @@ import 'package:equatable/equatable.dart';
 
 class GetCustodianBankStatusParams extends Equatable {
   final String bankId;
-  const GetCustodianBankStatusParams({required this.bankId});
+  final String? custodianBankStatusId;
+  const GetCustodianBankStatusParams({
+    required this.bankId,
+    required this.custodianBankStatusId,
+  });
 
   factory GetCustodianBankStatusParams.fromJson(Map<String, dynamic> json) =>
-      GetCustodianBankStatusParams(bankId: json['bankId']);
+      GetCustodianBankStatusParams(
+        bankId: json['bankId'],
+        custodianBankStatusId: json['custodianBankStatusId'],
+      );
 
-  Map<String, dynamic> toJson() => {'bankId': bankId};
+  Map<String, dynamic> toJson() => {
+        'bankId': bankId,
+        if (custodianBankStatusId != null)
+          'custodianBankStatusId': custodianBankStatusId,
+      };
 
   @override
   List<Object?> get props => [bankId];
