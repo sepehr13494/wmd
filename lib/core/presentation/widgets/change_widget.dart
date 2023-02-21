2ca417d15dfa3134f9fb9b98b7da7f7b08f8d918
sepehr <marash.sepehr@gmail.com>
2023-02-21 18:04:28 +0330

    tooltip on change widget

--- lib/core/presentation/widgets/change_widget.dart
+++ lib/core/presentation/widgets/change_widget.dart
@@ -1,12 +1,16 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:wmd/core/presentation/widgets/info_icon.dart';
 import 'package:wmd/core/util/colors.dart';
 
 class ChangeWidget extends StatelessWidget {
   final double number;
   final String text;
-  const ChangeWidget({Key? key, required this.number, required this.text})
+  final String? tooltipMessage;
+
+  const ChangeWidget(
+      {Key? key, required this.number, required this.text, this.tooltipMessage})
       : super(key: key);
 
   @override
@@ -30,6 +34,17 @@ class ChangeWidget extends StatelessWidget {
             text: isZero ? '0%' : text,
             style: TextStyle(color: color),
           ),
+          if (tooltipMessage != null)
+            const WidgetSpan(
+              child: Tooltip(
+                triggerMode: TooltipTriggerMode.tap,
+                message: "The performance computation maybe incorrect and the incoming data for the custodian bank needs to be checked",
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 4.0),
+                  child: InfoIcon(),
+                ),
+              ),
+            )
         ],
       ),
     );
--- lib/features/assets_overview/assets_overview/presentation/widgets/each_asset_type/inside_asset_card_mobile.dart
+++ lib/features/assets_overview/assets_overview/presentation/widgets/each_asset_type/inside_asset_card_mobile.dart
@@ -72,7 +72,9 @@ class InsideAssetCardMobile extends AppStatelessWidget {
                 children: [
                   ChangeWidget(
                       number: asset.inceptionToDate,
-                      text: "${asset.inceptionToDate.toStringAsFixed(1)}%"),
+                      text: "${asset.inceptionToDate.toStringAsFixed(1)}%",
+                    tooltipMessage: (asset.inceptionToDate > 99900 || asset.inceptionToDate < -100) ? "The performance computation maybe incorrect and the incoming data for the custodian bank needs to be checked" : null,
+                  ),
                   const SizedBox(width: 8),
                   ChangeWidget(
                       number: asset.yearToDate, text: "${asset.yearToDate.toStringAsFixed(1)} %"),
