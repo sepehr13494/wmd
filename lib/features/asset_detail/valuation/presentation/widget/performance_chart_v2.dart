596a39e29d883a33b85c26b279ad23ca799ca182
Ibrahim Mert Gul <ibrahimmertgul@gmail.com>
2023-02-27 15:01:36 +0300

    new numb format

--- lib/features/asset_detail/valuation/presentation/widget/performance_chart_v2.dart
+++ lib/features/asset_detail/valuation/presentation/widget/performance_chart_v2.dart
@@ -1,5 +1,4 @@
 import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:intl/intl.dart';
@@ -9,7 +8,7 @@ import 'package:wmd/core/extentions/num_ext.dart';
 import 'package:wmd/core/presentation/widgets/app_stateless_widget.dart';
 import 'package:wmd/core/presentation/widgets/responsive_helper/responsive_helper.dart';
 import 'package:wmd/core/util/colors.dart';
-
+import 'dart:ui' as ui;
 import 'performance_chart.dart';
 
 class PerformanceLineChartV2 extends AppStatelessWidget {
@@ -23,59 +22,28 @@ class PerformanceLineChartV2 extends AppStatelessWidget {
       BuildContext context, textTheme, AppLocalizations appLocalizations) {
     const int divider = 6;
     TooltipBehavior tooltipBehavior = TooltipBehavior(
-        enable: true,
-        // borderColor: Colors.red,
-        // borderWidth: 5,
-        builder: (data, point, series, pointIndex, seriesIndex) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text.rich(TextSpan(
-              text:
-                  CustomizableDateTime.localizedDdMmYyyy(data.key as DateTime),
-              style: textTheme.titleSmall!,
-              children: [
-                TextSpan(
-                    text: '\n${appLocalizations.assets_label_currentBalance}',
-                    style: textTheme.bodyMedium),
-                TextSpan(
-                    // ignore: prefer_interpolation_to_compose_strings
-                    text:
-                        ' ${(data.value as double).formatNumberWithDecimal()}',
-                    style: textTheme.titleSmall!
-                        .apply(color: AppColors.chartColor)),
-              ],
-            )),
-          );
-        },
-        color: const Color.fromARGB(255, 38, 49, 52));
-    double minY = 0;
-    if (values.isNotEmpty) {
-      minY = values[0].value;
-      for (var element in values) {
-        if (element.value < minY) {
-          minY = element.value;
-        }
-      }
-    }
-    double maxY = 0;
-    if (values.isNotEmpty) {
-      maxY = values[0].value;
-      for (var element in values) {
-        if (element.value > maxY) {
-          maxY = element.value;
-        }
-      }
-    }
-    late final double x;
-    if (maxY - minY == 0) {
-      if (maxY > 0) {
-        x = maxY;
-      } else {
-        x = -maxY;
-      }
-    } else {
-      x = maxY - minY / divider;
-    }
+      enable: true,
+      builder: (data, point, series, pointIndex, seriesIndex) {
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text.rich(TextSpan(
+            text: CustomizableDateTime.localizedDdMmYyyy(data.key as DateTime),
+            style: textTheme.titleSmall!,
+            children: [
+              TextSpan(
+                  text: '\n${appLocalizations.assets_label_currentBalance}',
+                  style: textTheme.bodyMedium),
+              TextSpan(
+                  // ignore: prefer_interpolation_to_compose_strings
+                  text: ' ${(data.value as double).formatCurrencyCompact()}',
+                  style:
+                      textTheme.titleSmall!.apply(color: AppColors.chartColor)),
+            ],
+          )),
+        );
+      },
+      color: const Color.fromARGB(255, 38, 49, 52),
+    );
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -110,7 +78,7 @@ class PerformanceLineChartV2 extends AppStatelessWidget {
                       labelStyle: textTheme.bodySmall!.apply(fontSizeDelta: -3),
                       edgeLabelPlacement: EdgeLabelPlacement.shift,
                       labelIntersectAction: AxisLabelIntersectAction.wrap,
-                      crossesAt: maxY > 0 ? 0 : null,
+                      crossesAt: 0,
                       placeLabelsNearAxisLine: false,
                     ),
                     primaryYAxis: NumericAxis(
@@ -118,27 +86,8 @@ class PerformanceLineChartV2 extends AppStatelessWidget {
                       labelStyle: textTheme.bodySmall!.apply(fontSizeDelta: -3),
                       majorGridLines: const MajorGridLines(width: 1),
                     ),
-                    // primaryYAxis: CategoryAxis(
-                    //   labelStyle: textTheme.bodySmall,
-                    //   axisLine: const AxisLine(width: 0),
-                    //   axisLabelFormatter: (axisLabelRenderArgs) {
-                    //     log('Mertlog:  ${axisLabelRenderArgs.value}');
-                    //     return ChartAxisLabel(
-                    //         axisLabelRenderArgs.value.formatNumberWithDecimal(),
-                    //         textTheme.bodySmall);
-                    //   },
-                    //   minimum: minY - x,
-                    //   maximum: maxY + x,
-                    //   // majorTickLines: const MajorTickLines(size: 1),
-                    //   majorGridLines: const MajorGridLines(width: 1),
-                    // ),
                     series: _getSeries(),
                     tooltipBehavior: tooltipBehavior,
-                    // tooltipBehavior: TooltipBehavior(
-                    //   enable: true,
-                    //   header: 'bksjdhfgkljshdfgljkshd',
-                    //   canShowMarker: true,
-                    // ),
                   );
                 }),
         ),
@@ -149,17 +98,63 @@ class PerformanceLineChartV2 extends AppStatelessWidget {
   _getSeries() {
     return [
       AreaSeries<MapEntry<DateTime, double>, String>(
-        color: AppColors.chartColor.withOpacity(0.3),
+        // color: AppColors.chartColor.withOpacity(0.3),
         borderColor: AppColors.chartColor,
+
         borderWidth: 2,
         dataSource: values,
         animationDuration: 2500,
         enableTooltip: true,
         xValueMapper: (vs, _) => CustomizableDateTime.localizedDdMm(vs.key),
         yValueMapper: (vs, _) => vs.value,
+        pointColorMapper: (datum, index) {
+          if (datum.value > 0) return AppColors.chartColor;
+          return AppColors.redChartColor;
+        },
+        gradient: LinearGradient(
+          colors: <Color>[
+            AppColors.chartColor.withOpacity(0.3),
+            AppColors.redChartColor.withOpacity(0.3),
+          ],
+          stops: <double>[0.1, 0.3, 0.5, 0.7, 0.9],
+          // Setting alignment for the series gradient
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+        ),
+        // onCreateRenderer: (series) {
+        //   return _CustomAreaSeriesRenderer(series);
+        // },
         markerSettings: MarkerSettings(
             isVisible: values.length == 1, color: AppColors.chartColor),
       ),
     ];
   }
 }
+
+// class _CustomAreaSeriesRenderer extends AreaSeriesRenderer {
+//   _CustomAreaSeriesRenderer(this.series);
+
+//   final ChartSeries<MapEntry<DateTime, double>, String> series;
+
+//   @override
+//   AreaSegment createSegment() => _AreaCustomPainter(series);
+// }
+
+// class _AreaCustomPainter extends AreaSegment {
+//   _AreaCustomPainter(this.series);
+
+//   final ChartSeries<MapEntry<DateTime, double>, String> series;
+//   @override
+//   int get currentSegmentIndex => super.currentSegmentIndex!;
+
+//   @override
+//   Paint getFillPaint() {
+//     final Paint customerFillPaint = Paint();
+//     log('Mert log:   $currentSegmentIndex');
+//     customerFillPaint.color = series.dataSource![currentSegmentIndex].value > 0
+//         ? AppColors.chartColor.withOpacity(0.3)
+//         : AppColors.redChartColor.withOpacity(0.3);
+//     customerFillPaint.style = PaintingStyle.fill;
+//     return customerFillPaint;
+//   }
+// }
