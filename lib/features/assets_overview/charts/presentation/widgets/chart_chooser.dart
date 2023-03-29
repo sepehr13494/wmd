895841d8cbffdc2f6450e4773d6afc5bee73536b
sepehr <marash.sepehr@gmail.com>
2023-03-29 04:36:58 +0430

    bug fix

--- lib/features/assets_overview/charts/presentation/widgets/chart_chooser.dart
+++ lib/features/assets_overview/charts/presentation/widgets/chart_chooser.dart
@@ -1,19 +1,21 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:wmd/core/presentation/widgets/app_stateless_widget.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:wmd/core/util/constants.dart';
 import 'package:wmd/features/assets_overview/charts/presentation/manager/chart_chooser_manager.dart';
 
-abstract class BarType{}
-enum AssetsBarType implements BarType{
+abstract class BarType {}
+
+enum AssetsBarType implements BarType {
   barChart,
   areaChart,
   areaPercentage,
 }
 
-enum GeoBarType implements BarType{
+enum GeoBarType implements BarType {
   map,
   tree,
 }
@@ -21,30 +23,44 @@ enum GeoBarType implements BarType{
 class AllChartType extends Equatable {
   final String name;
   final BarType barType;
+  final String image;
 
   const AllChartType({
     required this.name,
     required this.barType,
+    required this.image,
   });
 
-  static List<AllChartType> getAllTypes(BuildContext context, {bool isGeo = false}) {
+  static List<AllChartType> getAllTypes(BuildContext context,
+      {bool isGeo = false}) {
     final appLocalizations = AppLocalizations.of(context);
-    if(isGeo){
+    if (isGeo) {
       return [
-        AllChartType(name: appLocalizations.assets_charts_allocationCharts_treemapLabel, barType: GeoBarType.tree),
+        AllChartType(
+            name: appLocalizations.assets_charts_allocationCharts_treemapLabel,
+            barType: GeoBarType.tree,
+            image: "assets/images/area_chart.svg"),
         AllChartType(
             name: appLocalizations.assets_charts_allocationCharts_worldmapLabel,
-            barType: GeoBarType.map),
+            barType: GeoBarType.map,
+            image: "assets/images/area_chart.svg"),
       ];
-    }else{
+    } else {
       return [
         AllChartType(
             name: appLocalizations.assets_charts_allocationCharts_barChartLabel,
-            barType: AssetsBarType.barChart),
+            barType: AssetsBarType.barChart,
+            image: "assets/images/bar_chart.svg"),
         AllChartType(
-            name: appLocalizations.assets_charts_allocationCharts_areaChartLabel,
-            barType: AssetsBarType.areaChart),
-        AllChartType(name: "%${appLocalizations.assets_charts_allocationCharts_areaChartLabel}", barType: AssetsBarType.areaPercentage),
+            name:
+                appLocalizations.assets_charts_allocationCharts_areaChartLabel,
+            barType: AssetsBarType.areaChart,
+            image: "assets/images/area_chart.svg"),
+        AllChartType(
+            name:
+                "%${appLocalizations.assets_charts_allocationCharts_areaChartLabel}",
+            barType: AssetsBarType.areaPercentage,
+            image: "assets/images/area_percentage.svg"),
       ];
     }
   }
@@ -58,7 +74,9 @@ class AllChartType extends Equatable {
 class ChartChooserWidget extends StatefulWidget {
   final bool isGeo;
   final bool show;
-  const ChartChooserWidget({Key? key, required this.isGeo,this.show=true}) : super(key: key);
+
+  const ChartChooserWidget({Key? key, required this.isGeo, this.show = true})
+      : super(key: key);
 
   @override
   AppState<ChartChooserWidget> createState() => _ChartChooserWidgetState();
@@ -66,15 +84,17 @@ class ChartChooserWidget extends StatefulWidget {
 
 class _ChartChooserWidgetState extends AppState<ChartChooserWidget> {
   late ChartChooserManager provider;
+
   @override
   void didChangeDependencies() {
-    if(widget.isGeo){
+    if (widget.isGeo) {
       provider = context.watch<GeoChartChooserManager>();
-    }else{
+    } else {
       provider = context.watch<AssetChartChooserManager>();
     }
     if (provider.state == null) {
-      provider.changeChart(AllChartType.getAllTypes(context,isGeo: widget.isGeo).first);
+      provider.changeChart(
+          AllChartType.getAllTypes(context, isGeo: widget.isGeo).first);
     }
     super.didChangeDependencies();
   }
@@ -94,17 +114,46 @@ class _ChartChooserWidgetState extends AppState<ChartChooserWidget> {
           ),
           const SizedBox(width: 8),
           Builder(builder: (context) {
-            final items = AllChartType.getAllTypes(context,isGeo: widget.isGeo);
+            final items =
+                AllChartType.getAllTypes(context, isGeo: widget.isGeo);
             return DropdownButtonHideUnderline(
               child: DropdownButton<AllChartType>(
+                menuMaxHeight: 500,
+                  itemHeight: 120,
+                  selectedItemBuilder: (context) => items
+                      .map((e) => Text(
+                            e.name,
+                            style: textTheme.bodyMedium!
+                                .apply(color: Theme.of(context).primaryColor),
+                          ))
+                      .toList(),
                   isDense: true,
                   items: List.generate(items.length, (index) {
                     return DropdownMenuItem<AllChartType>(
                       value: items[index],
-                      child: Text(
-                        items[index].name,
-                        style: textTheme.bodyMedium!
-                            .apply(color: Theme.of(context).primaryColor),
+                      child: Container(
+                        width: 220,
+                        padding: const EdgeInsets.all(8),
+                        margin: const EdgeInsets.symmetric(vertical: 4),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            border:
+                                Border.all(color: Theme.of(context).cardColor)),
+                        child: Center(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              SvgPicture.asset(items[index].image,
+                                  width: 40, height: 40),
+                              const SizedBox(height: 4),
+                              Text(
+                                items[index].name,
+                                style: textTheme.bodyMedium!
+                                    .apply(color: Theme.of(context).primaryColor),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
                     );
                   }),
