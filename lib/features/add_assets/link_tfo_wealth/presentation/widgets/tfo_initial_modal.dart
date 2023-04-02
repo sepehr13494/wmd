92b60379f12de39108f4863ac78551e8d5883f28
Ibrahim Mert Gul <ibrahimmertgul@gmail.com>
2023-04-02 15:51:24 +0300

    add tablet view

--- lib/features/add_assets/link_tfo_wealth/presentation/widgets/tfo_initial_modal.dart
+++ lib/features/add_assets/link_tfo_wealth/presentation/widgets/tfo_initial_modal.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:wmd/core/extentions/text_style_ext.dart';
 import 'package:wmd/core/presentation/widgets/bottom_modal_widget.dart';
+import 'package:wmd/core/presentation/widgets/responsive_helper/responsive_helper.dart';
 
 Future<bool> showTfoInitialModal({
   required BuildContext context,
@@ -10,10 +11,11 @@ Future<bool> showTfoInitialModal({
   final appLocalizations = AppLocalizations.of(context);
   final textTheme = Theme.of(context).textTheme;
   final primaryColor = Theme.of(context).primaryColor;
+  final isMobile = ResponsiveHelper(context: context).isMobile;
   return await showDialog(
     context: context,
     builder: (context) {
-      return CenterModalWidget(
+      final content = CenterModalWidget(
         body: const Text('Do you want to load you existing TFO portfolio?'),
         actions: Padding(
           padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
@@ -41,6 +43,17 @@ Future<bool> showTfoInitialModal({
             ],
           ),
         ),
+      );
+      if (isMobile) {
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: content,
+        );
+      }
+      return Padding(
+        padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width / 2),
+        child: content,
       );
     },
   ).then((isConfirm) {
