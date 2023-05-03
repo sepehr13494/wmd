7b5d4513665b5e630b710bcdae92ec07346648f9
Mert Gül <22999961+imertgul@users.noreply.github.com>
2023-05-03 15:43:57 +0300

    page states

--- lib/features/settings/linked_accounts/presentation/page/linked_accounts_page.dart
+++ lib/features/settings/linked_accounts/presentation/page/linked_accounts_page.dart
@@ -1,12 +1,13 @@
 import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:wmd/core/presentation/bloc/base_cubit.dart';
 import 'package:wmd/core/presentation/bloc/bloc_helpers.dart';
 import 'package:wmd/core/presentation/widgets/app_stateless_widget.dart';
 import 'package:wmd/core/presentation/widgets/loading_widget.dart';
 import 'package:wmd/core/presentation/widgets/responsive_helper/responsive_helper.dart';
+import 'package:wmd/features/settings/linked_accounts/domain/entities/get_linked_accounts_entity.dart';
 import 'package:wmd/injection_container.dart';
 
 import '../manager/linked_accounts_cubit.dart';
@@ -28,62 +29,30 @@ class LinkedAccountsPage extends AppStatelessWidget {
             if (state is LoadingState) {
               return const LoadingWidget();
             }
+
             return Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
               child: SingleChildScrollView(
                 child: Column(
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          appLocalizations.profile_tabs_linkedAccounts_name,
-                          style: textTheme.headlineSmall,
-                        ),
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Theme.of(context).cardColor,
-                            border: Border.all(
-                                color: Theme.of(context).dividerColor),
-                          ),
-                          child: Padding(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 8.0),
-                            child: DropdownButtonHideUnderline(
-                              child: DropdownButton<String>(
-                                items: [
-                                  DropdownMenuItem<String>(
-                                    value: 'Show all',
-                                    child: Row(
-                                      children: [
-                                        Text(
-                                          'Show all',
-                                          style: textTheme.bodyMedium!,
-                                          // textTheme.bodyMedium!.toLinkStyle(context),
-                                        ),
-                                        const SizedBox(width: 16),
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                                onChanged: ((value) async {}),
-                                value: 'Show all',
-                                icon: Icon(
-                                  Icons.keyboard_arrow_down,
-                                  size: 15,
-                                  color: primaryColor,
-                                ),
-                                // style: textTheme.labelLarge,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
+                    _buildHeader(
+                        appLocalizations, textTheme, context, primaryColor),
                     const SizedBox(height: 8),
-                    isMobile
-                        ? const LinkedTableMobile()
-                        : const LinkedTableTablet(),
+                    Builder(builder: (context) {
+                      if (state is GetLinkedAccountsLoaded) {
+                        return isMobile
+                            ? LinkedTableMobile(
+                                getLinkedAccountsEntities:
+                                    state.getLinkedAccountsEntities)
+                            : LinkedTableTablet(
+                                getLinkedAccountsEntities:
+                                    state.getLinkedAccountsEntities);
+                      } else if (state is ErrorState) {
+                        return Text(state.failure.message);
+                      } else {
+                        return const SizedBox();
+                      }
+                    }),
                     const SizedBox(height: 16),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.start,
@@ -101,10 +70,60 @@ class LinkedAccountsPage extends AppStatelessWidget {
           }),
     );
   }
+
+  Row _buildHeader(AppLocalizations appLocalizations, TextTheme textTheme,
+      BuildContext context, Color primaryColor) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          appLocalizations.profile_tabs_linkedAccounts_name,
+          style: textTheme.headlineSmall,
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).cardColor,
+            border: Border.all(color: Theme.of(context).dividerColor),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton<String>(
+                items: [
+                  DropdownMenuItem<String>(
+                    value: 'Show all',
+                    child: Row(
+                      children: [
+                        Text(
+                          'Show all',
+                          style: textTheme.bodyMedium!,
+                          // textTheme.bodyMedium!.toLinkStyle(context),
+                        ),
+                        const SizedBox(width: 16),
+                      ],
+                    ),
+                  ),
+                ],
+                onChanged: ((value) async {}),
+                value: 'Show all',
+                icon: Icon(
+                  Icons.keyboard_arrow_down,
+                  size: 15,
+                  color: primaryColor,
+                ),
+                // style: textTheme.labelLarge,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
 
 class LinkedTableTablet extends AppStatelessWidget {
-  const LinkedTableTablet({super.key});
+  final List<GetLinkedAccountsEntity> getLinkedAccountsEntities;
+  const LinkedTableTablet({required this.getLinkedAccountsEntities, super.key});
 
   static const columnWidths = {
     0: IntrinsicColumnWidth(),
@@ -182,7 +201,8 @@ class LinkedTableTablet extends AppStatelessWidget {
 }
 
 class LinkedTableMobile extends AppStatelessWidget {
-  const LinkedTableMobile({super.key});
+  final List<GetLinkedAccountsEntity> getLinkedAccountsEntities;
+  const LinkedTableMobile({required this.getLinkedAccountsEntities, super.key});
 
   static const columnWidths = {
     0: IntrinsicColumnWidth(),
