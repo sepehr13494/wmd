83b8312cb62c8b4dca6c85692abd829304060b3c
sepehr <marash.sepehr@gmail.com>
2023-05-07 13:00:14 +0430

    performance table bug fix

--- lib/features/dashboard/performance_table/presentation/models/performance_value_obj.dart
+++ lib/features/dashboard/performance_table/presentation/models/performance_value_obj.dart
@@ -1,9 +1,11 @@
 class PerformanceValueObj {
   final String value;
   final bool shouldBlur;
+  final bool showTooltip;
 
   PerformanceValueObj({
     required this.value,
     this.shouldBlur = true,
+    this.showTooltip = false,
   });
 }
