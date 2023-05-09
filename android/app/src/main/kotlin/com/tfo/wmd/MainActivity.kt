fb499500e7f8b55787b1e1cd7e84411501fcbbf1
Mert Gül <22999961+imertgul@users.noreply.github.com>
2023-05-09 16:10:23 +0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/tfo/wmd/MainActivity.kt
+++ android/app/src/main/kotlin/com/tfo/wmd/MainActivity.kt
@@ -1,5 +1,65 @@
-package com.tfo.wmd
+// package com.tfo.wmd
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import android.provider.Settings;
 
-import io.flutter.embedding.android.FlutterFragmentActivity
+class MainActivity: FlutterActivity() {
+  private val CHANNEL = "adb"
 
-class MainActivity: FlutterFragmentActivity() {}
+  override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    super.configureFlutterEngine(flutterEngine)
+    MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+      call, result ->
+        if (call.method == "checkingadb") {
+            checkingadb(result);
+        } else {
+            result.notImplemented();
+        }
+    }
+  }
+
+    private fun checkingadb(  result: MethodChannel.Result) {
+        if (Settings.Secure.getInt(this.getContentResolver(), Settings.Secure.ADB_ENABLED, 0) == 1) {
+            // debugging enabled
+            result.success(1);
+        } else {
+            //;debugging does not enabled
+            result.success(0);
+        }
+    }
+}
+
+
+
+// import androidx.annotation.NonNull
+// import io.flutter.embedding.android.FlutterActivity
+// import io.flutter.embedding.engine.FlutterEngine
+// import io.flutter.plugin.common.MethodChannel
+
+// class MainActivity: FlutterActivity() {
+//   private val CHANNEL = "adb"
+
+//   override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+//     super.configureFlutterEngine(flutterEngine)
+//     MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+//       call, result ->
+//         if (call.method.equals("checkingadb")) {
+//             checkingadb(call,result);
+//         } else {
+//             result.notImplemented();
+//         }
+//     }
+//   }
+
+//       private fun checkingadb(): Int {
+//            if (Settings.Secure.getInt(this.getContentResolver(), Settings.Secure.ADB_ENABLED, 0) == 1) {
+//             // debugging enabled
+//             return 1;
+//         } else {
+//             //;debugging does not enabled
+//             return 0;
+//         }
+//       }
+// }
