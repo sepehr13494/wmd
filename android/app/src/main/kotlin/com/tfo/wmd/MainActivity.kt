0c7787ab65935b922bad784fdff47dcfffba61e6
Ibrahim Mert Gul <ibrahimmertgul@gmail.com>
2023-05-16 13:27:10 +0300

    fix mainActivity.kt

--- android/app/src/main/kotlin/com/tfo/wmd/MainActivity.kt
+++ android/app/src/main/kotlin/com/tfo/wmd/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity: FlutterActivity() {
             checkingadb(result);
         } else {
             result.notImplemented();
-        
+        }
     }
   }
 
@@ -32,36 +32,3 @@ class MainActivity: FlutterActivity() {
         }
     }
 }
-
-
-
-// import androidx.annotation.NonNull
-// import io.flutter.embedding.android.FlutterActivity
-// import io.flutter.embedding.engine.FlutterEngine
-// import io.flutter.plugin.common.MethodChannel
-
-// class MainActivity: FlutterActivity() {
-//   private val CHANNEL = "adb"
-
-//   override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-//     super.configureFlutterEngine(flutterEngine)
-//     MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
-//       call, result ->
-//         if (call.method.equals("checkingadb")) {
-//             checkingadb(call,result);
-//         } else {
-//             result.notImplemented();
-//         }
-//     }
-//   }
-
-//       private fun checkingadb(): Int {
-//            if (Settings.Secure.getInt(this.getContentResolver(), Settings.Secure.ADB_ENABLED, 0) == 1) {
-//             // debugging enabled
-//             return 1;
-//         } else {
-//             //;debugging does not enabled
-//             return 0;
-//         }
-//       }
-// }
