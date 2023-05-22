8f2c1cccc190d2fc23e369cdf16645e317cc0c2c
Mert Gül <22999961+imertgul@users.noreply.github.com>
2023-05-22 13:24:24 +0300

    clear mert logs

--- lib/features/liability_overview/presentation/widget/liability_list.dart
+++ lib/features/liability_overview/presentation/widget/liability_list.dart
@@ -1,12 +1,27 @@
+import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wmd/core/presentation/bloc/bloc_helpers.dart';
 import 'package:wmd/core/presentation/widgets/app_stateless_widget.dart';
+
+import '../../../../injection_container.dart';
+import '../manager/liablility_overview_cubit.dart';
 
 class LiabilityList extends AppStatelessWidget {
   const LiabilityList({super.key});
 
   @override
   Widget buildWidget(BuildContext context, textTheme, appLocalizations) {
-    return Container();
+    return BlocProvider(
+      create: (context) =>
+          sl<LiabilityOverviewCubit>()..getLiablilityOverview(),
+      child: BlocConsumer<LiabilityOverviewCubit, LiablilityOverviewState>(
+          listener: BlocHelper.defaultBlocListener(listener: (context, state) {
+        log('Mert log: $state');
+      }), builder: (context, state) {
+        return Text(state.toString());
+      }),
+    );
   }
 }
