7d4920b36a495fb2293f305a9d1eb2960b4b50f7
Mert Gül <22999961+imertgul@users.noreply.github.com>
2023-06-13 14:21:52 +0300

    add delete test cases

--- test/features/dashboard/mandate_status/presentation/manager/mandate_status_cubit_test.dart
+++ test/features/dashboard/mandate_status/presentation/manager/mandate_status_cubit_test.dart
@@ -4,12 +4,10 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:wmd/core/error_and_success/failures.dart';
-
+import 'package:wmd/core/error_and_success/succeses.dart';
 import 'package:wmd/core/presentation/bloc/base_cubit.dart';
-import 'package:wmd/features/dashboard/mandate_status/data/models/get_mandate_status_params.dart';
-import 'package:wmd/features/dashboard/mandate_status/data/models/get_mandate_status_response.dart';
+import 'package:wmd/features/dashboard/mandate_status/domain/use_cases/delete_mandate_usecase.dart';
 import 'package:wmd/features/dashboard/mandate_status/domain/use_cases/get_mandate_status_usecase.dart';
-import 'package:wmd/features/dashboard/mandate_status/presentation/manager/mandate_status_cubit.dart';
 
 import 'mandate_status_cubit_test.mocks.dart';
 
@@ -17,19 +15,23 @@ import 'mandate_status_cubit_test.mocks.dart';
 
 @GenerateMocks([
   GetMandateStatusUseCase,
+  DeleteMandateUseCase,
 
 ])
 void main() {
   late MockGetMandateStatusUseCase mockGetMandateStatusUseCase;
+  late MockDeleteMandateUseCase mockDeleteMandateUseCase;
 
   late MandateStatusCubit mandateStatusCubit;
 
 
   setUp(() {
     mockGetMandateStatusUseCase = MockGetMandateStatusUseCase();
+    mockDeleteMandateUseCase = MockDeleteMandateUseCase();
 
     mandateStatusCubit = MandateStatusCubit(
     mockGetMandateStatusUseCase,
+    mockDeleteMandateUseCase,
 
     );
   });
@@ -59,6 +61,33 @@ void main() {
       [isA<LoadingState>(), ErrorState(failure: ServerFailure.tServerFailure)],
       verify: (_) {
         verify(mockGetMandateStatusUseCase(GetMandateStatusParams.tParams));
+      },
+    );
+  });
+  group("deleteMandate", () {
+    blocTest(
+      'when DeleteMandateUseCase emits the DeleteMandateLoaded when success',
+      build: () => mandateStatusCubit,
+      setUp: () => when(mockDeleteMandateUseCase(any))
+          .thenAnswer((realInvocation) async => Right(AppSuccess(message: "Successfully done"))),
+      act: (bloc) async => await bloc.deleteMandate(),
+      expect: () =>
+      [isA<LoadingState>(), SuccessState(appSuccess: AppSuccess.tAppSuccess)],
+      verify: (_) {
+        verify(mockDeleteMandateUseCase(DeleteMandateParams.tParams));
+      },
+    );
+
+    blocTest(
+      'when DeleteMandateUseCase emits the ErrorState when error',
+      build: () => mandateStatusCubit,
+      setUp: () => when(mockDeleteMandateUseCase(any))
+          .thenAnswer((realInvocation) async => const Left(ServerFailure.tServerFailure)),
+      act: (bloc) async => await bloc.deleteMandate(),
+      expect: () =>
+      [isA<LoadingState>(), ErrorState(failure: ServerFailure.tServerFailure)],
+      verify: (_) {
+        verify(mockDeleteMandateUseCase(DeleteMandateParams.tParams));
       },
     );
   });
