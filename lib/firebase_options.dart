d67711193cbd1d9077440b9e87a9ea1271c85629
Hashan Jayalath <hashan.jayalath@nagarro.com>
2023-06-19 13:26:57 +0400

    ios configs

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -63,7 +63,8 @@ class DefaultFirebaseOptions {
     messagingSenderId: '332478051043',
     projectId: 'wmdproject-5cb71',
     storageBucket: 'wmdproject-5cb71.appspot.com',
-    iosClientId: '332478051043-e2vqllv5cu7dar76h2uel33jbntfvr7n.apps.googleusercontent.com',
+    iosClientId:
+        '332478051043-e2vqllv5cu7dar76h2uel33jbntfvr7n.apps.googleusercontent.com',
     iosBundleId: 'com.tfo.wmd',
   );
 }
