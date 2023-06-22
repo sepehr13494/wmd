43b7df9c3c6fc8b36b674e71a93f2b904262b095
Hashan Jayalath <hashan.jayalath@nagarro.com>
2023-06-22 12:43:21 +0400

    android config added

--- android/app/src/main/kotlin/com/tfo/wmd/MainActivity.kt
+++ android/app/src/main/kotlin/com/tfo/wmd/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.tfo.wmd
+package com.aiop.aio
 import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
