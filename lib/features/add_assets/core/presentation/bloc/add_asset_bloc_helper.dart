c588254758bd555dae00ba0a42d3609e38e654d1
Hashan Jayalath <hashan.jayalath@nagarro.com>
2023-07-16 09:59:50 +0400

    Fix: 32226

--- lib/features/add_assets/core/presentation/bloc/add_asset_bloc_helper.dart
+++ lib/features/add_assets/core/presentation/bloc/add_asset_bloc_helper.dart
@@ -3,13 +3,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wmd/core/extentions/num_ext.dart';
 import 'package:wmd/core/presentation/bloc/bloc_helpers.dart';
 import 'package:wmd/core/presentation/routes/app_router.dart';
+import 'package:wmd/core/util/constants.dart';
 import 'package:wmd/features/add_assets/core/presentation/bloc/add_asset_base_state.dart';
 import 'package:wmd/features/add_assets/core/presentation/widgets/success_modal.dart';
 import 'package:wmd/features/add_assets/core/presentation/widgets/success_modal_onboarding.dart';
-import 'package:wmd/features/assets_overview/assets_overview/presentation/manager/assets_overview_cubit.dart';
-import 'package:wmd/features/dashboard/dashboard_charts/presentation/manager/dashboard_allocation_cubit.dart';
-import 'package:wmd/features/dashboard/dashboard_charts/presentation/manager/dashboard_goe_cubit.dart';
-import 'package:wmd/features/dashboard/dashboard_charts/presentation/manager/dashboard_pie_cubit.dart';
 import 'package:wmd/features/dashboard/main_dashbaord/presentation/manager/main_dashboard_cubit.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -22,6 +19,7 @@ class AssetBlocHelper extends BlocHelper {
     return BlocHelper.defaultBlocListener(
       listener: (context, state) {
         final appLocalizations = AppLocalizations.of(context);
+
         if (state is AddAssetState) {
           /*context.read<MainDashboardCubit>().initPage();
           context.read<AssetsOverviewCubit>().initPage();
@@ -74,10 +72,8 @@ class AssetBlocHelper extends BlocHelper {
                 );
               }
               return SuccessModalOnboardingWidget(
-                title: appLocalizations
-                    .common_formSuccessModal_newUser_description
-                    .replaceAll('{{assetName}}', asset)
-                    .replaceAll("{{assetType}}", assetType),
+                title:
+                    AssetLoclHelper.getName(asset, assetType, appLocalizations),
                 confirmBtn:
                     appLocalizations.common_formSuccessModal_buttons_viewAsset,
                 cancelBtn:
@@ -93,5 +89,54 @@ class AssetBlocHelper extends BlocHelper {
         }
       },
     );
+  }
+}
+
+class AssetLoclHelper {
+  static String getName(
+      asset, String assetType, AppLocalizations appLocalizations) {
+    final title = appLocalizations.common_formSuccessModal_newUser_description;
+    String assetName = "";
+
+    switch (assetType) {
+      case AssetTypes.bankAccount:
+        assetName = appLocalizations.assets_assets_BankAccount;
+        break;
+      case AssetTypes.listedAsset:
+        assetName = appLocalizations.assets_assets_ListedAsset;
+        break;
+      case AssetTypes.listedAssetEquity:
+        assetName = appLocalizations.assets_assets_ListedAsset;
+        break;
+      case AssetTypes.listedAssetFixedIncome:
+        assetName = appLocalizations.assets_assets_ListedAsset;
+        break;
+      case AssetTypes.listedAssetOther:
+        assetName = appLocalizations.assets_assets_ListedAsset;
+        break;
+      case AssetTypes.listedAssetOtherAsset:
+        assetName = appLocalizations.assets_assets_ListedAsset;
+        break;
+      case AssetTypes.otherAsset:
+        assetName = appLocalizations.assets_assets_OtherAssets;
+        break;
+      case AssetTypes.otherAssets:
+        assetName = appLocalizations.assets_assets_OtherAssets;
+        break;
+      case AssetTypes.privateDebt:
+        assetName = appLocalizations.assets_assets_PrivateDebt;
+        break;
+      case AssetTypes.privateEquity:
+        assetName = appLocalizations.assets_assets_PrivateEquity;
+        break;
+      case AssetTypes.realEstate:
+        assetName = appLocalizations.assets_assets_RealEstate;
+        break;
+      default:
+    }
+
+    return title
+        .replaceAll('{{assetName}}', "")
+        .replaceAll("{{assetType}}", assetName);
   }
 }
