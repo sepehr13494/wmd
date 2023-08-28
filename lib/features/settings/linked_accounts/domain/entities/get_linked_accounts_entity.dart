fc2570b31706fa388a6fa21419cd679af3c40a86
Mert Gül <22999961+imertgul@users.noreply.github.com>
2023-08-28 13:20:35 +0300

    new status added

--- lib/features/settings/linked_accounts/domain/entities/get_linked_accounts_entity.dart
+++ lib/features/settings/linked_accounts/domain/entities/get_linked_accounts_entity.dart
@@ -1,9 +1,12 @@
 import 'package:equatable/equatable.dart';
+import 'package:wmd/features/add_assets/custodian_bank_auth/domain/entities/get_custodian_bank_status_entity.dart';
 
 class GetLinkedAccountsEntity extends Equatable {
   final String id;
   final String bankName;
   final DateTime dateLinked;
+  final DateTime? syncDate;
+  final CustodianStatus status;
   final String type;
   final String subType;
   const GetLinkedAccountsEntity(
@@ -11,10 +14,18 @@ class GetLinkedAccountsEntity extends Equatable {
       required this.id,
       required this.dateLinked,
       required this.type,
+       this.syncDate,
+      required this.status,
       required this.subType});
 
   Map<String, dynamic> toJson() => {};
 
   @override
-  List<Object?> get props => [];
+  List<Object?> get props => [
+        id,
+        bankName,
+        dateLinked,
+        type,
+        subType,
+      ];
 }
