cf0312cf8b5705e0f3091244e8b3e0028c327c39
Ibrahim Mert Gul <ibrahimmertgul@gmail.com>
2023-09-14 15:52:24 +0300

    add form validators

--- lib/features/add_assets/request_new_custodian/presentation/widget/new_custodian_request_modal.dart
+++ lib/features/add_assets/request_new_custodian/presentation/widget/new_custodian_request_modal.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:wmd/core/presentation/bloc/bloc_helpers.dart';
 import 'package:wmd/core/presentation/widgets/app_stateless_widget.dart';
 import 'package:wmd/core/presentation/widgets/bottom_modal_widget.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:wmd/core/util/colors.dart';
+import 'package:wmd/features/add_assets/core/data/models/country.dart';
+import 'package:wmd/features/add_assets/request_new_custodian/presentation/manager/request_new_custodian_cubit.dart';
+import 'package:wmd/injection_container.dart';
 
 Future<void> showNewCustodianModal({
   required BuildContext context,
@@ -22,149 +27,192 @@ Future<void> showNewCustodianModal({
   );
 }
 
-class RequestCustodianForm extends AppStatelessWidget {
+class RequestCustodianForm extends StatefulWidget {
   const RequestCustodianForm({
     Key? key,
   }) : super(key: key);
 
   @override
+  AppState<RequestCustodianForm> createState() => _RequestCustodianFormState();
+}
+
+class _RequestCustodianFormState extends AppState<RequestCustodianForm> {
+  bool checkbox = false;
+  final _formKey = GlobalKey<FormBuilderState>();
+
+  @override
   Widget buildWidget(BuildContext context, textTheme, appLocalizations) {
-    final _formKey = GlobalKey<FormBuilderState>();
-    return FormBuilder(
-      key: _formKey,
-      onChanged: () {
-        _formKey.currentState!.save();
-        debugPrint(_formKey.currentState!.value.toString());
-      },
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              width: double.infinity,
-              child: Text(
-                appLocalizations.common_newCustodianRequest_modal_title,
-                style: textTheme.titleMedium,
-                textAlign: TextAlign.center,
-              ),
-            ),
-            const SizedBox(height: 16),
-            Text(appLocalizations.common_newCustodianRequest_modal_bankDetails),
-            const SizedBox(height: 8),
-            FormBuilderTextField(
-              autovalidateMode: AutovalidateMode.always,
-              name: 'accountNumber',
-              decoration: InputDecoration(
-                labelText: appLocalizations
-                    .common_newCustodianRequest_modal_accountNumber,
-                filled: true,
-                fillColor: AppColors.bgColor,
-                border: InputBorder.none,
-              ),
-              keyboardType: TextInputType.text,
-              textInputAction: TextInputAction.next,
-            ),
-            const SizedBox(height: 8),
-            FormBuilderTextField(
-              autovalidateMode: AutovalidateMode.always,
-              name: 'bankName',
-              decoration: InputDecoration(
-                labelText:
-                    appLocalizations.common_newCustodianRequest_modal_bankName,
-                filled: true,
-                fillColor: AppColors.bgColor,
-                border: InputBorder.none,
-              ),
-              keyboardType: TextInputType.text,
-              textInputAction: TextInputAction.next,
-            ),
-            const SizedBox(height: 8),
-            FormBuilderTextField(
-              autovalidateMode: AutovalidateMode.always,
-              name: 'bankCountry',
-              decoration: InputDecoration(
-                labelText: appLocalizations
-                    .common_newCustodianRequest_modal_bankCountry,
-                filled: true,
-                fillColor: AppColors.bgColor,
-                border: InputBorder.none,
-              ),
-              keyboardType: TextInputType.text,
-              textInputAction: TextInputAction.next,
-            ),
-            const SizedBox(height: 24),
-            Text(appLocalizations
-                .common_newCustodianRequest_modal_managerDetails),
-            const SizedBox(height: 8),
-            FormBuilderTextField(
-              autovalidateMode: AutovalidateMode.always,
-              name: 'managerName',
-              decoration: InputDecoration(
-                labelText: appLocalizations
-                    .common_newCustodianRequest_modal_managerName,
-                filled: true,
-                fillColor: AppColors.bgColor,
-                border: InputBorder.none,
-              ),
-              keyboardType: TextInputType.text,
-              textInputAction: TextInputAction.next,
-            ),
-            const SizedBox(height: 8),
-            FormBuilderTextField(
-              autovalidateMode: AutovalidateMode.always,
-              name: 'managerEmail',
-              decoration: InputDecoration(
-                labelText: appLocalizations
-                    .common_newCustodianRequest_modal_managerEmail,
-                filled: true,
-                fillColor: AppColors.bgColor,
-                border: InputBorder.none,
-              ),
-              keyboardType: TextInputType.text,
-              textInputAction: TextInputAction.next,
-            ),
-            const SizedBox(height: 24),
-            Card(
-              color: AppColors.blueCardColor,
-              margin: const EdgeInsets.all(0),
+    final validator = FormBuilderValidators.compose([
+      FormBuilderValidators.required(
+          errorText: appLocalizations.common_errors_required),
+    ]);
+
+    return BlocProvider(
+      create: (context) => sl<RequestNewCustodianCubit>(),
+      child: BlocConsumer<RequestNewCustodianCubit, RequestNewCustodianState>(
+          listener:
+              BlocHelper.defaultBlocListener(listener: (context, state) {}),
+          builder: (context, state) {
+            return FormBuilder(
+              key: _formKey,
+              onChanged: () {
+                _formKey.currentState!.save();
+                debugPrint(_formKey.currentState!.value.toString());
+              },
               child: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Checkbox(
-                      value: true,
-                      onChanged: (val) {},
-                    ),
-                    Expanded(
+                    SizedBox(
+                      width: double.infinity,
                       child: Text(
-                        appLocalizations.common_newCustodianRequest_modal_check,
-                        style: textTheme.bodySmall,
+                        appLocalizations.common_newCustodianRequest_modal_title,
+                        style: textTheme.titleMedium,
+                        textAlign: TextAlign.center,
                       ),
                     ),
+                    const SizedBox(height: 16),
+                    Text(appLocalizations
+                        .common_newCustodianRequest_modal_bankDetails),
+                    const SizedBox(height: 8),
+                    FormBuilderTextField(
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      name: 'accountNumber',
+                      decoration: InputDecoration(
+                        labelText: appLocalizations
+                            .common_newCustodianRequest_modal_accountNumber,
+                        filled: true,
+                        fillColor: AppColors.bgColor,
+                        border: InputBorder.none,
+                      ),
+                      validator: validator,
+                      keyboardType: TextInputType.text,
+                      textInputAction: TextInputAction.next,
+                    ),
+                    const SizedBox(height: 8),
+                    FormBuilderTextField(
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      name: 'bankName',
+                      decoration: InputDecoration(
+                        labelText: appLocalizations
+                            .common_newCustodianRequest_modal_bankName,
+                        filled: true,
+                        fillColor: AppColors.bgColor,
+                        border: InputBorder.none,
+                      ),
+                      validator: validator,
+                      keyboardType: TextInputType.text,
+                      textInputAction: TextInputAction.next,
+                    ),
+                    const SizedBox(height: 8),
+                    FormBuilderDropdown(
+                      name: 'country',
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: validator,
+                      items: Country.getCountryList(context)
+                          .map((e) => DropdownMenuItem(
+                                value: e.name,
+                                child: Text(e.countryName),
+                              ))
+                          .toList(),
+                      decoration: InputDecoration(
+                        labelText: appLocalizations
+                            .common_newCustodianRequest_modal_bankCountry,
+                        filled: true,
+                        fillColor: AppColors.bgColor,
+                        border: InputBorder.none,
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                    Text(appLocalizations
+                        .common_newCustodianRequest_modal_managerDetails),
+                    const SizedBox(height: 8),
+                    FormBuilderTextField(
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      name: 'rmName',
+                      decoration: InputDecoration(
+                        labelText: appLocalizations
+                            .common_newCustodianRequest_modal_managerName,
+                        filled: true,
+                        fillColor: AppColors.bgColor,
+                        border: InputBorder.none,
+                      ),
+                      validator: validator,
+                      keyboardType: TextInputType.text,
+                      textInputAction: TextInputAction.next,
+                    ),
+                    const SizedBox(height: 8),
+                    FormBuilderTextField(
+                      name: 'rmEmail',
+                      decoration: InputDecoration(
+                        labelText: appLocalizations
+                            .common_newCustodianRequest_modal_managerEmail,
+                        filled: true,
+                        fillColor: AppColors.bgColor,
+                        border: InputBorder.none,
+                      ),
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: validator,
+                      keyboardType: TextInputType.text,
+                      textInputAction: TextInputAction.next,
+                    ),
+                    const SizedBox(height: 24),
+                    Card(
+                      color: AppColors.blueCardColor,
+                      margin: const EdgeInsets.all(0),
+                      child: FormBuilderCheckbox(
+                        name: 'checkbox',
+                        selected: checkbox,
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        validator: FormBuilderValidators.compose([
+                          FormBuilderValidators.required(
+                              errorText:
+                                  appLocalizations.common_errors_required),
+                          FormBuilderValidators.notEqual(false,
+                              errorText:
+                                  appLocalizations.common_errors_required),
+                        ]),
+                        onChanged: (val) {
+                          if (val != null) {
+                            setState(() {
+                              checkbox = val;
+                            });
+                          }
+                        },
+                        title: Text(
+                          appLocalizations
+                              .common_newCustodianRequest_modal_check,
+                          style: textTheme.bodySmall,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const SizedBox(height: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              print('not nulll');
+                            } else {
+                              print('Cna not check valid');
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                              minimumSize: const Size(100, 50)),
+                          child: Text(appLocalizations.common_button_submit),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 16),
                   ],
                 ),
               ),
-            ),
-            const SizedBox(height: 8),
-            const SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                ElevatedButton(
-                  onPressed: () => Navigator.pop(context, true),
-                  style: ElevatedButton.styleFrom(
-                      minimumSize: const Size(100, 50)),
-                  child: Text(appLocalizations.common_button_submit),
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
-          ],
-        ),
-      ),
+            );
+          }),
     );
   }
 }
