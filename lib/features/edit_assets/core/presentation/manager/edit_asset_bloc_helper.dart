b6386aa3e547e4ea3d2259c27b64b5a1cbc4a43a
Mert Gül <22999961+imertgul@users.noreply.github.com>
2023-09-23 23:41:14 +0300

    edit asset names ln for popups

--- lib/features/edit_assets/core/presentation/manager/edit_asset_bloc_helper.dart
+++ lib/features/edit_assets/core/presentation/manager/edit_asset_bloc_helper.dart
@@ -3,6 +3,7 @@ import 'package:go_router/go_router.dart';
 import 'package:wmd/core/presentation/bloc/bloc_helpers.dart';
 import 'package:wmd/core/presentation/routes/app_router.dart';
 import 'package:wmd/core/presentation/routes/app_routes.dart';
+import 'package:wmd/core/util/constants.dart';
 import 'package:wmd/features/asset_detail/core/data/models/get_summary_params.dart';
 import 'package:wmd/features/asset_detail/core/presentation/manager/asset_summary_cubit.dart';
 import 'package:wmd/features/assets_overview/assets_overview/presentation/manager/assets_overview_cubit.dart';
@@ -10,23 +11,39 @@ import 'package:wmd/features/edit_assets/core/presentation/manager/edit_asset_st
 import 'package:wmd/global_functions.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class EditAssetBlocHelper{
+class EditAssetBlocHelper {
   static BlocWidgetListener defaultBlocListener({
     required String assetId,
     required String type,
   }) {
-    return BlocHelper.defaultBlocListener(listener: (context, state) {
-      if (state is EditAssetSuccess) {
-        context.read<AssetSummaryCubit>().getSummary(GetSummaryParams(days: 7, assetId: assetId));
-        AppRouter().setMainRefreshKey();
-        GlobalFunctions.showSnackBar(context, AppLocalizations.of(context).assetLiabilityForms_toast_assetEditSuccess.replaceAll("{{assetName}}", type),type: "success");
-        context.pop();
-      }
-      if (state is DeleteAssetSuccess) {
-        GlobalFunctions.showSnackBar(context, AppLocalizations.of(context).assetLiabilityForms_toast_assetDeleteSuccess.replaceAll("{{assetName}}", type),type: "success");
-        AppRouter().setMainRefreshKey();
-        context.goNamed(AppRoutes.main);
-      }
-    },);
+    return BlocHelper.defaultBlocListener(
+      listener: (context, state) {
+        final appLocalizations = AppLocalizations.of(context);
+        final typeName = AssetTypes.getAssetType(appLocalizations, type);
+        if (state is EditAssetSuccess) {
+          context
+              .read<AssetSummaryCubit>()
+              .getSummary(GetSummaryParams(days: 7, assetId: assetId));
+          AppRouter().setMainRefreshKey();
+          GlobalFunctions.showSnackBar(
+              context,
+              AppLocalizations.of(context)
+                  .assetLiabilityForms_toast_assetEditSuccess
+                  .replaceAll("{{assetName}}", typeName),
+              type: "success");
+          context.pop();
+        }
+        if (state is DeleteAssetSuccess) {
+          GlobalFunctions.showSnackBar(
+              context,
+              AppLocalizations.of(context)
+                  .assetLiabilityForms_toast_assetDeleteSuccess
+                  .replaceAll("{{assetName}}", typeName),
+              type: "success");
+          AppRouter().setMainRefreshKey();
+          context.goNamed(AppRoutes.main);
+        }
+      },
+    );
   }
 }
